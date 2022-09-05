3f197dcfe52aee31890dada201fdc71c6ed75eb0
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-05 10:24:30 +0700

    Adding constants :sparkles:

--- src/App.js
+++ src/App.js
@@ -2,6 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 import React from 'react';
 
+import {
+  ChannelDetail,
+  Feed,
+  Navbar,
+  SearchFeed,
+  VideoDetail,
+} from './components';
+
 const App = () => (
   <BrowserRouter>
     <Box sx={{ backgroundColor: '#000' }}>
