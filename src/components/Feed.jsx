c5417b852fe4acb5f870543ee64264e664d54fb5
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-05 16:33:07 +0700

    Adding Videos component :sparkles:

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -21,6 +21,19 @@ const Feed = () => {
           Copyright 2022 JSM Media
         </Typography>
       </Box>
+
+      <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: 'white' }}
+        >
+          New <span style={{ color: '#F31503' }}>videos</span>
+        </Typography>
+
+        <Videos />
+      </Box>
     </Stack>
   );
 };
