3b79e08daec340ef9645621e9aed08facab54ee2
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-05 16:27:48 +0700

    Editing Sidebar :hammer:

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,7 +1,43 @@
-import React from 'react';
+import { Stack } from '@mui/material';
 
-const SideBar = () => {
-  return <div>SideBar</div>;
-};
+import { categories } from '../utils/constants';
 
-export default SideBar;
+const selectedCategory = 'New';
+
+const Sidebar = () => (
+  <Stack
+    direction="row"
+    sx={{
+      overflowY: 'auto',
+      height: { sx: 'auto', md: '95%' },
+      flexDirection: { md: 'column' },
+    }}
+  >
+    {categories.map((category) => (
+      <button
+        className="category-btn"
+        style={{
+          background: category.name === selectedCategory && '#fC1503',
+          color: 'white',
+        }}
+        key={category.name}
+      >
+        <span
+          style={{
+            color: category.name === selectedCategory ? 'white' : 'red',
+            marginRight: '15px',
+          }}
+        >
+          {category.icon}
+        </span>
+        <span
+          style={{ opacity: category.name === selectedCategory ? '1' : '0.8' }}
+        >
+          {category.name}
+        </span>
+      </button>
+    ))}
+  </Stack>
+);
+
+export default Sidebar;
--- src/components/index.js
+++ src/components/index.js
@@ -4,4 +4,4 @@ export { default as Feed } from './Feed';
 export { default as SearchFeed } from './SearchFeed';
 export { default as VideoDetail } from './VideoDetail';
 export { default as SearchBar } from './SearchBar';
-export { default as SideBar } from './SideBar';
+export { default as Sidebar } from './Sidebar';
