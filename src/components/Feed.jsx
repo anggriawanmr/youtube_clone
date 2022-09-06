8771bac853ad9e2564d0bd22e77da763cb318307
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-06 10:45:20 +0700

    Editing VideoCard component :hammer:

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Box, Stack, Typography } from '@mui/material';
 
-import { Sidebar, Videos } from './';
+import { Sidebar, Videos, Loader } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const Feed = () => {
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Stack, Box } from '@mui/material';
 
-import { VideoCard, ChannelCard } from './';
+import { ChannelCard, Loader, VideoCard } from './';
 
 const Videos = ({ videos }) => {
   return (
-    <Stack direction="row" flexWrap="wrap" justifyContent="start" gap={2}>
-      {videos.map((item, idx) => {
+    <Stack
+      direction="row"
+      flexWrap="wrap"
+      justifyContent="start"
+      alignItems="start"
+      gap={2}
+    >
+      {videos.map((item, idx) => (
         <Box key={idx}>
           {item.id.videoId && <VideoCard video={item} />}
           {/* {item.id.channelId && <ChannelCard channelDetail={item} />} */}
-        </Box>;
-      })}
+        </Box>
+      ))}
     </Stack>
   );
 };
