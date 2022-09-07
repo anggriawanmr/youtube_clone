d1f8f802764f9410fdb1cf56f4555c2c6d52c7c5
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-08 01:58:48 +0700

    Editing VideoDetail component :hammer:

--- src/components/VideoDetail.jsx
+++ src/components/VideoDetail.jsx
@@ -4,19 +4,31 @@ import ReactPlayer from 'react-player';
 import { Typography, Box, Stack } from '@mui/material';
 import { CheckCircle } from '@mui/icons-material';
 
-import { Video } from './';
+import { Videos } from './';
 import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const VideoDetail = () => {
   const [videoDetail, setVideoDetail] = useState(null);
+  const [videos, setVideos] = useState(null);
 
   const { id } = useParams();
 
   useEffect(() => {
     fetchFromAPI(`videos?part=snippet,statistics&id=${id}`).then((data) =>
-      setVideoDetail(data.item[0])
+      setVideoDetail(data.items[0])
+    );
+
+    fetchFromAPI(`search?part=snippet&relatedToVideoId=${id}&type=video`).then(
+      (data) => setVideos(data.items)
     );
   }, [id]);
+
+  if (!videoDetail?.snippet) return 'Loading...';
+
+  const {
+    snippet: { title, channelId, channelTitle },
+    statistics: { viewCount, likeCount },
+  } = videoDetail;
 
   return (
     <Box minHeight="95vh">
@@ -28,8 +40,45 @@ const VideoDetail = () => {
               className="react-player"
               controls
             />
-            <Typography></Typography>
+            <Typography color="#fff" variant="h5" fontWeight="bold" p={2}>
+              {title}
+            </Typography>
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              sx={{ color: '#fff' }}
+              py={1}
+              px={2}
+            >
+              <Link to={`/channel/${channelId}`}>
+                <Typography
+                  variant={{ sm: 'subtitle1', md: 'h6' }}
+                  color="#fff"
+                >
+                  {channelTitle}
+                  <CheckCircle
+                    sx={{ fontSize: '12px', color: 'gray', ml: '5px' }}
+                  />
+                </Typography>
+              </Link>
+              <Stack direction="row" gap="20px" alignItems="center">
+                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                  {parseInt(viewCount).toLocaleString()} Views
+                </Typography>
+                <Typography variant="body1" sx={{ opacity: 0.7 }}>
+                  {parseInt(likeCount).toLocaleString()} Likes
+                </Typography>
+              </Stack>
+            </Stack>
           </Box>
+        </Box>
+        <Box
+          px={2}
+          py={{ md: 1, xs: 5 }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          <Videos videos={videos} direction="column" />
         </Box>
       </Stack>
     </Box>
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -3,10 +3,12 @@ import { Stack, Box } from '@mui/material';
 
 import { ChannelCard, Loader, VideoCard } from './';
 
-const Videos = ({ videos }) => {
+const Videos = ({ videos, direction }) => {
+  if (!videos?.length) return 'Loading...';
+
   return (
     <Stack
-      direction="row"
+      direction={direction || 'row'}
       flexWrap="wrap"
       justifyContent="start"
       alignItems="start"
