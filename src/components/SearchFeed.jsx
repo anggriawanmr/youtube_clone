6942e5ccc5a66c6d33df6023de5ad3b3318daa62
Anggriawan Ridatama <anggriawanmr@gmail.com>
2022-09-07 13:15:57 +0700

    Editing SearchFeed :hammer:

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, Typography } from '@mui/material';
+import {useParams} from 'react-router-dom';
+
+import { Videos } from './';
+import { fetchFromAPI } from '../utils/fetchFromAPI';
 
 const SearchFeed = () => {
-  return <div>SearchFeed</div>;
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, []);
+
+  return (
+    <Box p={2} sx={{ overflowY: 'auto', height: '90vh', flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: 'white' }}>
+        Search Results for: <span style={{ color: '#F31503' }}>videos</span>
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
+  );
 };
 
 export default SearchFeed;
